558e9daa36257b50057ff7bd68b7a08b24765af4
Sequencer <45060278+sequencerr@users.noreply.github.com>
2024-11-04 19:45:51 +0100

    Push firefox user.js

--- home/.mozilla/firefox/profile/user.js
+++ home/.mozilla/firefox/profile/user.js
@@ -1,4 +1,12 @@
-user_pref("accessibility.typeaheadfind.enablesound", false);
-user_pref("extensions.screenshots.disabled", true);
-user_pref("ui.key.menuAccessKeyFocuses", false);
-user_pref("browser.quitShortcut.disabled", true);
+user_pref('accessibility.typeaheadfind.enablesound', false);
+user_pref('extensions.screenshots.disabled', true);
+user_pref('ui.key.menuAccessKeyFocuses', false);
+user_pref('browser.quitShortcut.disabled', true);
+user_pref('devtools.selfxss.count', 6); // https://www.reddit.com/r/firefox/comments/jbvog6/comment/g8zftf9
+user_pref('extensions.formautofill.creditCards.enabled', false);
+user_pref('browser.aboutConfig.showWarning', false);
+user_pref('signon.rememberSignons', false);
+user_pref('extensions.pocket.enabled', false);
+user_pref('browser.sessionstore.resume_from_crash', true);
+user_pref('ui.context_menus.after_mouseup', true);
+user_pref('browser.startup.page', 3); // https://github.com/mozilla/gecko-dev/blob/91112db840238a7dfc15361f4762ba8bf4560806/browser/components/preferences/main.js#L242
